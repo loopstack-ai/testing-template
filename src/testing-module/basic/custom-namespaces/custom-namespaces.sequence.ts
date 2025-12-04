c6acdb2e970ab0edbc9ecb80f5ec31618bc4f8ef
Jakob <jakob.klippel@config.one>
2025-12-04 11:19:35 +0100

    fix: add missing imports

--- src/testing-module/basic/custom-namespaces/custom-namespaces.sequence.ts
+++ src/testing-module/basic/custom-namespaces/custom-namespaces.sequence.ts
@@ -2,9 +2,10 @@ import { BlockConfig } from '@loopstack/common';
 import { Pipeline } from '@loopstack/core';
 import { VegetablesSequence } from './vegetables/vegetables.sequence';
 import { FruitsSequence } from './fruits/fruits.sequence';
+import { HelloFromNamespaceMessageWorkflow } from './workflows/hello-from-namespace-message.workflow';
 
 @BlockConfig({
-  imports: [VegetablesSequence, FruitsSequence],
+  imports: [HelloFromNamespaceMessageWorkflow, VegetablesSequence, FruitsSequence],
   configFile: __dirname + '/custom-namespaces.sequence.yaml',
 })
 export class CustomNamespacesSequence extends Pipeline {}
